93d0b60a06036cbe41f9700ce3127b1eb118399b
SuperNovalx <hoanganhgo9@hotmail.com>
2020-07-07 11:53:31 +0700

    Initial

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import pandas as pd
+
+app = Flask(__name__)
+
+df = pd.read_csv('data.csv')
+
+
+@app.route('/employee/<employee_id>')
+def get_employee(employee_id):
+    if employee_id.isnumeric():
+        for index, row in df.iterrows():
+            if row['ID'] == int(employee_id):
+                return df.loc[index].to_json()
+    return '{}'
+
+
+
+if __name__ == '__main__':
+    app.run()
